d6a9f7bd8ddbb342c4cbfe1da5e0c682d0426bc6
Patrick Bentley <pbentley07@gmail.com>
2018-06-11 21:00:28 +0300

    Adds Testing For Web3 and ABI

--- compile.js
+++ compile.js
@@ -5,4 +5,9 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-solc.compile(source, 1); //! 1 = number of contracts compiled 
+
+module.exports=solc.compile(source, 1).contracts[':Inbox']; //? ,1 = number of contracts compiled 
+//! .contracts[':Inbox'] breaks off the compile Object to view the contracts and specifically the Inbox contract since it is the only contract we are using in this example
+//! use solc.compile(source, 1) to see the entire compile object if curious
+
+
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,18 +2,25 @@ const assert = require('assert');
 const ganache = require('ganache-cli'); //? CLI for creating a local Ethereum Network 
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider()); //? provider creates a connection between web3 and local ethereum network (ganache)
+const { interface, bytecode } = require('../compile'); 
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+
+beforeEach(async () => {
     //? Get a list of 'open' accounts on ganache for testing 
-    web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-        console.log(fetchedAccounts);
-    })
+    accounts = await web3.eth.getAccounts();//? Gets open Accounts for Contract Deployment fro Ganache CLI
+
+    inbox  = await  new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi There']})//? Deploys using bytecode from Compile file -- Arguments is the Inital Message which is necessary to deploy new contract
+    .send({ from: accounts[0], gas: ' 1000000' }) //? Sends form open account from Ganache and sets a gas limit 
 });
 
 describe('inbox contracts', () => {
     it('deploys contract', () => {
+        console.log(inbox);
+    });
 
-    }); 
 });
   
